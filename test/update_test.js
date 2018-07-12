2eaa09d641f9b9ecf71032642aa5b917024200de
jmshanahan <comeraghsolutions@gmail.com>
2018-07-12 16:24:23 +0100

    Implemented update modifier $inc and tested it

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ const User = require("user");
 describe("Updating records::", () => {
   let joe;
   beforeEach(done => {
-    joe = new User({ name: "Joe" });
+    joe = new User({ name: "Joe", postCount: 0 });
     joe
       .save()
       .then(() => done())
@@ -38,5 +38,14 @@ describe("Updating records::", () => {
   });
   it("Should update one record on a model class finding it with its ID", done => {
     assertName(User.findByIdAndUpdate(joe._id, { name: "Alex" }), done);
+  });
+  it.only("Should increment a users postcount by 1", done => {
+    User.update({ name: "Joe" }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: "Joe" }))
+      .then(user => {
+        expect(user.postCount).to.equal(1);
+        done();
+      })
+      .catch(err => done(err));
   });
 });
